00a57600fc594bedb75abf32b0ebcdc5a60e3502
AirCods <airscods@gmail.com>
2022-08-15 18:07:56 +0000

    добавлена функция поиска по постам

--- static/js/active.js
+++ static/js/active.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('.menu a').each(function (){
         let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
         let link = this.href;
-        if(link === location){
+        if(link == location){
             $(this).parent().addClass('active');
         }
     });
